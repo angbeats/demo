0d97756fdc16856af3e6e2e375c120ad7b0ac5c7
Angbeats <yangqisen@52tt.com>
2020-12-11 18:55:13 +0800

    Update Es demo

--- es-demo/src/main/java/com/my/qs/esdemo/config/elasticConfig.java
+++ es-demo/src/main/java/com/my/qs/esdemo/config/elasticConfig.java
@@ -1,7 +1,12 @@
 package com.my.qs.esdemo.config;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,10 +19,28 @@ public class elasticConfig {
 	private String host;
 
 	@Value("${es.http.port}")
-	private String port;
+	private int port;
+
+	@Value("${es.username}")
+	private String username;
+
+	@Value("${es.password}")
+	private String password;
 
 	@Bean
-	public RestHighLevelClient esHttpClient(){
-		return new RestHighLevelClient(RestClient.builder(new HttpHost(host, Integer.parseInt(port), "http")));
+	public RestHighLevelClient restHighLevelClient(){
+		BasicCredentialsProvider basicCredentialsProvider = new BasicCredentialsProvider();
+		basicCredentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+		return new RestHighLevelClient(
+				RestClient.builder(new HttpHost(host, port))
+						.setHttpClientConfigCallback(
+								new RestClientBuilder.HttpClientConfigCallback() {
+									@Override
+									public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+										return httpAsyncClientBuilder.setDefaultCredentialsProvider(basicCredentialsProvider);
+									}
+								}
+						)
+		);
 	}
 }
